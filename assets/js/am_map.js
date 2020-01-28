3e3b4c009ffecb872cc501925b435a8c97a79277
Sophie Spatharioti <s.spatharioti@gmail.com>
2020-01-28 15:06:16 -0500

    map fixes, thesis prop updates

--- assets/js/am_map.js
+++ assets/js/am_map.js
@@ -3,35 +3,37 @@
  * http://kevinurrutia.tumblr.com/post/16411271583/creating-a-css3-pulsating-circle
  */
 
+ //http://www.latlong.net/
  var markers = [
-  //Europe
- {lat: 37.9833333, lng: 23.727539, name: "Athens, Greece"},
- {lat: 48.856614, lng: 2.352222, name:"Paris, France" },
- {lat: 41.902783, lng: 12.496366, name: "Rome, Italy" },
- {lat: 45.070312, lng: 7.686856, name: "Turin, Italy" },
- {lat: 40.416775, lng: -3.703790, name:"Madrid, Spain"},
- {lat: 51.507351, lng: -0.127758, name: "London, UK" },
- {lat: 50.909700, lng: -1.404351, name: "Southampton, UK" },
- {lat: 43.925085, lng: 2.148641, name: "Albi, France" },
- //USA
- {lat: 42.360082, lng: -71.058880, name: "Boston, USA" },
- {lat: 37.338208, lng: -121.886329, name: "San Jose, CA"},
- {lat: 30.267153, lng: -97.743061, name: "Austin, TX"},
- {lat: 25.761680, lng: -80.191790, name: "Miami, FL"},
- {lat: 47.606209, lng: -122.332071, name:"Seattle, WA"},
- {lat: 40.712784, lng: -74.005941, name:"New York, NY"},
- {lat: 44.943888, lng: -93.099710, name:"St Paul, MN"},
- {lat: 41.499320, lng: -81.694361, name:"Cleveland, OH"},
- {lat: 34.052234, lng: -118.243685, name:"Los Angeles, CA"},
- {lat: 29.951066, lng: -90.071532, name:"New Orleans, LA"},
- {lat: 35.779591, lng: -78.638176, name:"Raleigh, NC"},
- {lat:41.878113, lng: -87.629799 , name:"Chicago, IL"},
- //Rest:
- {lat: 9.392308, lng: -84.136988, name:" Costa Rica"},
- {lat: 20.967370, lng: -89.592586, name:" Mexico"},
- {lat: 25.034281, lng: -77.396278, name:"The Bahamas"}
-
-]
+   //Europe
+   {lat: 37.9833333, lng: 23.7333333, name: "Athens, Greece"},
+   {lat: 48.856614, lng: 2.352222, name:"Paris, France" },
+   {lat: 41.902783, lng: 12.496366, name: "Rome, Italy" },
+   {lat: 45.070312, lng: 7.686856, name: "Turin, Italy" },
+   {lat: 40.416775, lng: -3.703790, name:"Madrid, Spain"},
+   {lat: 51.507351, lng: -0.127758, name: "London, UK" },
+   {lat: 50.909700, lng: -1.404351, name: "Southampton, UK" },
+   {lat: 43.925085, lng: 2.148641, name: "Albi, France" },
+   {lat: 41.385063, lng: 2.173404, name: "Barcelona, Spain" },
+   //USA
+   {lat: 42.360082, lng: -71.058880, name: "Boston, USA" },
+   {lat: 37.338208, lng: -121.886329, name: "San Jose, CA"},
+   {lat: 30.267153, lng: -97.743061, name: "Austin, TX"},
+   {lat: 25.761680, lng: -80.191790, name: "Miami, FL"},
+   {lat: 47.606209, lng: -122.332071, name:"Seattle, WA"},
+   {lat: 40.712784, lng: -74.005941, name:"New York, NY"},
+   {lat: 44.943888, lng: -93.099710, name:"St Paul, MN"},
+   {lat: 41.499320, lng: -81.694361, name:"Cleveland, OH"},
+   {lat: 34.052234, lng: -118.243685, name:"Los Angeles, CA"},
+   {lat: 29.951066, lng: -90.071532, name:"New Orleans, LA"},
+   {lat: 35.779591, lng: -78.638176, name:"Raleigh, NC"},
+   {lat:41.878113, lng: -87.629799 , name:"Chicago, IL"},
+   {lat:21.306944, lng: -157.858337, name:"Honolulu, HI"},
+   //Rest:
+   {lat: 9.392308, lng: -84.136988, name:" Costa Rica"},
+   {lat: 20.967370, lng: -89.592586, name:"Mexico"},
+   {lat: 25.034281, lng: -77.396278, name:"The Bahamas"}
+ ]
 
  //dot svg
  var targetSVG = "M368.19 208.41C368.19 222.2 356.99 233.41 343.19 233.41C329.39 233.41 318.19 222.2 318.19 208.41C318.19 194.61 329.39 183.41 343.19 183.41C356.99 183.41 368.19 194.61 368.19 208.41Z"
